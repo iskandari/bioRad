730a933aa177ddb5cb6015458bff1a66929732a3
adokter <adriaandokter@gmail.com>
2023-05-17 16:46:31 -0400

    move internal statement down, fixes build warning

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,3 @@
-#' @keywords internal
 #' A wrapper for spTransform
 #'
 #' This function calls spTransform, setting the sp_evolution_status option to 2L.
@@ -8,6 +7,7 @@
 #' @return The result of calling spTransform with the specified parameters and
 #'   sp_evolution_status set to 2L.
 #'
+#' @keywords internal
 #' @export
 spTransform2 <- function(...) {
   withr::with_options(list(sp_evolution_status = 2L), sp::spTransform(...))
